a2fb2f14073687ef966e744946f6169aa562e0c4
Frédéric Jacques <gary.bob88@gmail.com>
2021-07-11 21:05:01 -0400

    improve kibana script

--- kibana/deploy-kibana.ps1
+++ kibana/deploy-kibana.ps1
@@ -5,7 +5,15 @@ $resourceGroupName = "kibanatest"
 $clusterName = "clustbana"
 $location = "eastus"
 
+# Source:
+# https://www.elastic.co/blog/how-to-run-elastic-cloud-on-kubernetes-from-azure-kubernetes-service
+# https://www.elastic.co/guide/en/beats/metricbeat/current/metricbeat-installation-configuration.html
+
+# https://docs.microsoft.com/en-us/azure/virtual-machines/sizes-b-series-burstable
+
 $ErrorActionPreference = "Stop"
+
+az login
 
 Add-ResourceGroup $resourceGroupName $location
 
@@ -21,27 +29,55 @@ kubectl apply -f https://download.elastic.co/downloads/eck/1.1.2/all-in-one.yaml
 
 kubectl apply -f .\kibana\elasticsearch.yaml
 
+Start-Sleep 20
 kubectl get elasticsearch 
-Start-Sleep 15
 
+Start-Sleep 20
 kubectl get pods
-Start-Sleep 15
 
+Start-Sleep 20
 kubectl logs -f quickstart-es-default-0 
-Start-Sleep 15
 
+Start-Sleep 20
 kubectl get service quickstart-es-http
-Start-Sleep 15
 
-PASSWORD=$(kubectl get secret quickstart-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode)
+$PASSWORD=$(kubectl get secret quickstart-es-elastic-user -o=jsonpath='{.data.elastic}')
+$PASSWORD=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($PASSWORD))
 
 kubectl get svc quickstart-es-http
 
 Write-Output "Enter kibana IP address, you can find it in the log of the console : "
 $kibanaipaddress = Read-Host
 
-Invoke-WebRequest -u "elastic:$PASSWORD" -k "https://$($kibanaipaddress):9200"
+Write-Output $kibanaipaddress
+Write-Output $PASSWORD
+Write-Output "Invoke-WebRequest -ProxyCredential ""elastic:$PASSWORD"" -Proxy ""https://$($kibanaipaddress):9200"""
 
 kubectl apply -f .\kibana\kibana.yaml
 
 kubectl get kibana
+
+$vmAdminUser = "kibanaadmin"
+$vmName = "myVM"
+
+az vm create --resource-group $resourceGroupName --name $vmName --image UbuntuLTS --admin-username $vmAdminUser --generate-ssh-keys
+
+# ssh $vmAdminUser@<vm-public-ip>
+
+# wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.9.2-amd64.deb
+
+# sudo dpkg -i metricbeat-7.9.2-amd64.deb
+
+# sudo nano /etc/metricbeat/metricbeat.yml
+
+# sudo nano /etc/metricbeat/modules.d/azure.yml.disabled
+
+# Create the application in Azure AD and add a client secret
+
+# cd /usr/bin
+
+# sudo ./metricbeat modules enable azure
+
+# sudo ./metricbeat setup --dashboards # ssl error...
+
+# sudo ./metric
